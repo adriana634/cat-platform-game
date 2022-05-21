19792b48580eca54fb241cdc9cac82dac9ecc905
adriana634 <50183872+adriana634@users.noreply.github.com>
2022-05-21 16:28:47 +0200

    Off limits background

--- src/js/scenery_items.js
+++ src/js/scenery_items.js
@@ -1,4 +1,9 @@
 const sceneryItems = [{
+        x: -500,
+        y: 0,
+        image: 'background'
+    },
+    {
         x: 0,
         y: 0,
         image: 'background'
@@ -17,6 +22,11 @@ const sceneryItems = [{
         x: 1024,
         y: 0,
         image: 'hills'
+    },
+    {
+        x: 2048,
+        y: 0,
+        image: 'background'
     }
 ];
 
